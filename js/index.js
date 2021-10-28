4fa26f47e8cc98196f83e87974683f9f43f13a3c
Noa Udler <udlernoa4@gmail.com>
2021-10-28 15:56:37 -0700

    javascript changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
-function submitLibs(...stuff) {
-    console.log(stuff)
-    return false
+const allMadLibsContent = {};
+
+function onClick(event) {
+  console.log(event);
+  const allInputs = document
+    .querySelectorAll("input")
+    .forEach((input) => (allMadLibsContent[input.id] = input.value));
+  console.log(allMadLibsContent);
+  window.location.href = "form.html";
+  return false;
 }
+
+function viewMadlib() {
+  const startingPoint = document.getElementById("story-starts"); 
+  startingPoint.innerHTML = "<p>`You are walking on a street ${allMadLibsContent["adj-1"]} and you did`></p>"
+}
+
+// function onClick(event) {
+//     const allInputs = document.querySelectorAll("input")
+//     for (let i = 0; i < allInputs; i++) {
+//         console.log(input[i].id + ": " + input[i].value)
+//     }
+// }
+// };
